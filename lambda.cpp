0529d7e2a7ee95e39433edbb12221cb948595fec
unknown <lijiang1@ztgame.com>
2017-06-01 14:05:21 +0800

    fix cmp function

--- lambda.cpp
+++ lambda.cpp
@@ -15,7 +15,7 @@ struct stTest
 	DWORD dwUserID;
 };
 
-void PrintOutStTestIfno(stTest(&arrayTest)[3])
+void PrintOutStTestInfo(stTest(&arrayTest)[3])
 {
 	for each (stTest var in arrayTest)
 	{
@@ -30,22 +30,22 @@ bool SortID(const stTest &val1, const stTest &val2)
 
 bool SortName(const stTest &val1, const stTest &val2)
 {
-	return val1.szUserName < val2.szUserName;
+	return strcmp(val1.szUserName, val2.szUserName) == 1 ? true : false;
 }
 
 int main()
 {
-	//Lambda
+	//Lambda way
  	stTest arrayTest[] = { stTest(10, "xsk"), stTest(20, "Axsk"), stTest(6, "test") };
 	std::sort(arrayTest, arrayTest + 3, [](const stTest &val1, stTest &val2){return val1.dwUserID < val2.dwUserID; });
-	PrintOutStTestIfno(arrayTest);
-	std::sort(arrayTest, arrayTest + 3, [](const stTest &val1, stTest &val2){return val1.szUserName < val2.szUserName; });
-	PrintOutStTestIfno(arrayTest);
+	PrintOutStTestInfo(arrayTest);
+	std::sort(arrayTest, arrayTest + 3, [](const stTest &val1, stTest &val2){return strcmp(val1.szUserName, val2.szUserName)== 1? true:false; });
+	PrintOutStTestInfo(arrayTest);
 
-	//传统方式
-	stTest arrayTest2[] = { stTest(22, "axsk6"), stTest(17, "Xmsk"), stTest(25, "test") };
+	//normal way
+	stTest arrayTest2[] = { stTest(22, "b"), stTest(17, "a"), stTest(25, "test") };
 	std::sort(arrayTest2, arrayTest2 + 3, SortID);
-	PrintOutStTestIfno(arrayTest2);
+	PrintOutStTestInfo(arrayTest2);
 	std::sort(arrayTest2, arrayTest2 + 3, SortName);
-	PrintOutStTestIfno(arrayTest2);
+	PrintOutStTestInfo(arrayTest2);
 }
